fc7c18e6f4de01f9d3cb7e34dc0ea43ebb5b4d7d
Endermanch67 <127907268+Endermanch67@users.noreply.github.com>
2024-06-05 16:46:16 -0500

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -417,7 +417,6 @@ void SCR_DisplayTicRate(void)
 	if (totaltics <= TICRATE / 2) ticcntcolor = V_REDMAP;
 else if (totaltics <= TICRATE * 0.90) ticcntcolor = V_YELLOWMAP;
 else ticcntcolor = V_GREENMAP;
-<<<<<<< HEAD
 
 	V_DrawString(vid.width-(24*vid.dupx), vid.height-(16*vid.dupy),
 		V_YELLOWMAP|V_NOSCALESTART, "FPS");
