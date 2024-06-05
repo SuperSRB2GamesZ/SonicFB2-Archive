dd1502033d13c37cc7456e07b69faa91bc53c740
Chromaticpipe <127907268+Endermanch67@users.noreply.github.com>
2024-06-05 09:33:24 -0500

    funy modversion

--- src/doomdef.h
+++ src/doomdef.h
@@ -209,15 +209,16 @@ extern FILE *logstream;
 #define CODEBASE 210
 
 // The Modification ID; must be obtained from Rob ( https://mb.srb2.org/private.php?do=newpm&u=546 ).
-// DO NOT try to set this otherwise, or your modification will be unplayable through the Master Server.
+// DO NOT try to set this otherwise, or your modification will be unplayable through the Master Server. 
+// WHO CARES ABOUT THE MASTER SERVER
 // "12" is the default mod ID for version 2.1
-#define MODID 12
+#define MODID 69
 
 // The Modification Version, starting from 1. Do not follow your version string for this,
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.1.0 is not version "1".
-#define MODVERSION 30
+#define MODVERSION 69
 
 // To version config.cfg, MAJOREXECVERSION is set equal to MODVERSION automatically.
 // Increment MINOREXECVERSION whenever a config change is needed that does not correspond
